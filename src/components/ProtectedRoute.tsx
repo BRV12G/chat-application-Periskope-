2a28302753480ccd96b9f3f5aa8ee1698c579c75
BRV12G <159134024+BRV12G@users.noreply.github.com>
2025-05-24 15:31:44 +0530

    feat: added the protected route

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { supabase } from '@/lib/supabaseClient'
+
+export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const [loading, setLoading] = useState(true)
+  const router = useRouter()
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser()
+
+      if (!user) {
+        router.push('/login')
+      } else {
+        setLoading(false)
+      }
+    }
+
+    checkUser()
+  }, [router])
+
+  if (loading) return <p className="p-4">Checking auth...</p>
+
+  return <>{children}</>
+}
